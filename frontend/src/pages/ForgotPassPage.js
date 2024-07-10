3ea650cabced463edd00a26180cc5e3138acfdcc
Wlphillips <Wlphillips.home@gmail.com>
2024-07-10 11:20:39 -0400

    Changed URL to match Daniel's Heroku App

--- frontend/src/pages/ForgotPassPage.js
+++ frontend/src/pages/ForgotPassPage.js
@@ -4,7 +4,7 @@ import AboutUsHeader from '../components/AboutUsHeader';
 import '../main.css';
 
 const ForgotPassPage = () =>{
-    const app_name = 'paradisepours-85b61313006b'
+    const app_name = 'paradise-pours-4be127640468'
     function buildPath(route)
     {
         if (process.env.NODE_ENV === 'production')
