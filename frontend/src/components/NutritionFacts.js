267bae27efba6fcd90e79f3a7a215eacbc923681
Jose Neto <jo651706@ucf.edu>
2024-07-12 20:32:27 -0400

    Nutrition facts layout changes

--- frontend/src/components/NutritionFacts.js
+++ frontend/src/components/NutritionFacts.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import FavButton from './FavButton.js';
+import '../main.css';
+import '../css/BeerPage.css';
 
 const NutritionFacts = ({beerToDisplay, switchComp}) => {
     return(
         <div>
             <h1>{beerToDisplay.Name}</h1>
             <div className = "beer-info">
-                <h1 className = "beer-info-header">Company: </h1> <p>{beerToDisplay.Company}</p>
-                <h1 className = "beer-info-header">Style:</h1> <p>{beerToDisplay.Style}</p>
-                <h1 className = "beer-info-header">ABV:</h1> <p>{beerToDisplay.ABV}%</p>
-                <h1 className = "beer-info-header">Calories:</h1> <p>{beerToDisplay.Calories}</p>
-                <h1 className = "beer-info-header">Origin:</h1> <p>{beerToDisplay.Origin}</p>
-                <h1 className = "beer-info-header">Favorite?</h1><div className = "fav-button-conatiner-in-list">{<FavButton currentBeer={beerToDisplay} />}</div>
+                <div className="nutrition-item">
+                    <h1 className="beer-info-header">Company: </h1> 
+                    <p>{beerToDisplay.Company}</p>
+                </div>
+                <div className="nutrition-item">
+                    <h1 className="beer-info-header">Style:</h1> 
+                    <p>{beerToDisplay.Style}</p>
+                </div>
+                <div className="nutrition-item">
+                    <h1 className="beer-info-header">ABV:</h1> 
+                    <p>{beerToDisplay.ABV}%</p>
+                </div>
+                <div className="nutrition-item">
+                    <h1 className="beer-info-header">Calories:</h1> 
+                    <p>{beerToDisplay.Calories}</p>
+                </div>
+                <div className="nutrition-item">
+                    <h1 className="beer-info-header">Origin:</h1> 
+                    <p>{beerToDisplay.Origin}</p>
+                </div>
+                <div className="nutrition-item">
+                    <h1 className="beer-info-header">Favorite?</h1>
+                    <div className="fav-button-container-in-list">
+                        <FavButton currentBeer={beerToDisplay} />
+                    </div>
+                </div>
                 <button onClick={switchComp} className = "ratings-button">Rate Beer<i className="bi bi-arrow-right"></i></button>
             </div>
         </div>
