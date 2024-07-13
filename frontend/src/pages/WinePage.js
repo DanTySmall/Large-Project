63d3d48c4e865a819b988e60956abffe591a9877
Jose Neto <jo651706@ucf.edu>
2024-07-13 18:52:59 -0400

    Fixed CSS

--- frontend/src/pages/WinePage.js
+++ frontend/src/pages/WinePage.js
@@ -1,6 +1,8 @@
 import WineHeader from "../components/WineHeader";
 import React, { } from 'react';
 import WineList from "../components/WineList";
+import '../css/WinePage.css';
+import '../main.css';
 
 function WinePage(){
     return(
