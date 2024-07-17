2006a9c2955accb4a234bd4a28b0dcc25f6f79a0
Jose Neto <jo651706@ucf.edu>
2024-07-17 14:40:51 -0400

    Fix attempt

--- API/beer.js
+++ API/beer.js
@@ -139,10 +139,27 @@ router.get('/getBeerComments', async (req, res) => {
     const { _id } = req.query; // Use req.query to get the parameter from the query string
     const BeerId = ObjectId.createFromHexString(_id);
 
-    const beer = await db.collection('Beer').findOne({ _id: BeerId });
+    // const beer = await db.collection('Beer').findOne({ _id: BeerId });
+    const result = await db.collection('Beer').aggregate([
+        { $match: { _id: BeerId } },
+        {
+            $project: {
+                _id: 1,
+                comments: {
+                    $cond: {
+                        if: { $gt: [{ $size: "$Ratings" }, 0] },
+                        then: "$Ratings",
+                        else: []
+                    }
+                }
+            }
+        }
+    ]).toArray();
 
-    if (beer) {
-        res.status(200).json({ comments: beer.Ratings });
+    if (result.length > 0) {
+        // res.status(200).json({ comments: beer.Ratings });
+        const comments = result[0].comments;
+        res.status(200).json({ comments });
     } else {
         res.status(400).json({ message: "Comments could not be retrieved." });
     }
