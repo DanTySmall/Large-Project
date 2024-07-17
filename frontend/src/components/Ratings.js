2abfd5b31107802b711f7b4dacd51a0ce545dd35
Jose Neto <jo651706@ucf.edu>
2024-07-17 12:40:03 -0400

    DOM Property

--- frontend/src/components/Ratings.js
+++ frontend/src/components/Ratings.js
@@ -106,7 +106,7 @@ const Ratings = ({beerToDisplay, switchComp}) => {
                 </div>
                 <div className="comment-container">
                     <input placeholder="Comment" className="comment-bar" type="text" onChange={(e) => setComment(e.target.value)} />
-                    <button className="comment-button" /*onClick={handleComment}*/ ><i class="bi bi-arrow-up-circle"></i></button>
+                    <button className="comment-button" /*onClick={handleComment}*/ ><i className="bi bi-arrow-up-circle"></i></button>
                 </div>
                 <button onClick={switchComp} className = "ratings-button"><i className="bi bi-arrow-left"></i>Back</button>
             </div>
